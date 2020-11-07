6514ac92d1a6aa8a12d97bc6695cbccb489caef1
Brandon Miller <brandonkmiller@protonmail.com>
2020-11-07 00:54:53 -0500

    TCP remote receive methods
    Wrote RecvUntil and RecvLine and unit tests

--- remote.go
+++ remote.go
@@ -1,30 +1,92 @@
 package sploit;
 
 import(
-	"net"
-	"strconv"
+    "net"
+    "strconv"
+    "bufio"
+    "io"
+    "errors"
+    "bytes"
 )
 
 // Remote is an interface for communicating over IP
 type Remote struct {
-	Host string
-	Port uint16
+    HostPort string
+    Host string
+    Port uint16
+    C net.Conn
 }
 
 // NewRemote connects to the specified remote host and returns an initialized Remote instance
-func NewRemote(hostPort string) (*Remote, error) {
-	host, portString, err := net.SplitHostPort(hostPort)
-	if err != nil {
-		return nil, err
-	}
+func NewRemote(protocol string, hostPort string) (*Remote, error) {
+    host, portString, err := net.SplitHostPort(hostPort)
+    if err != nil {
+        return nil, err
+    }
 
-	port, err := strconv.ParseUint(portString, 10, 16)
-	if err != nil {
-		return nil, err
-	}
+    port, err := strconv.ParseUint(portString, 10, 16)
+    if err != nil {
+        return nil, err
+    }
 
-	return &Remote{
-		Host: host,
-		Port: uint16(port),
-	}, nil
+    c, err := net.Dial(protocol, hostPort)
+    if err != nil {
+        return nil, err
+    }
+
+    return &Remote{
+        HostPort: hostPort,
+        Host: host,
+        Port: uint16(port),
+        C: c,
+    }, nil
+}
+
+// Close is a Remote method for closing the connection if it's active
+func (r *Remote)Close() {
+    if r.C != nil {
+        r.C.Close()
+    }
+}
+
+// RecvUntil is a Remote method for receiving data until the specified sequence of bytes is detected
+func (r *Remote)RecvUntil(needle []byte, drop bool)([]byte, error) {
+    data := make([]byte, len(needle))
+    b := bufio.NewReader(r.C)
+
+    // Read needle-size
+    n, err := io.ReadFull(b, data)
+    if err != nil {
+        return nil, err
+    }
+
+    // Make sure it read the entire size of the needle
+    if n != len(needle) {
+        return nil, errors.New("RecvUntil truncated")
+    }
+
+    // Compare needle and received data and continue to read a byte at a time
+    idx := 0
+    for {
+        if bytes.Compare(data[idx:idx+len(needle)], needle) == 0 {
+            if drop == true {
+                return data[0:len(data)-len(needle)], nil
+            } else {
+                return data, nil
+            }
+        }
+
+        byt, err := b.ReadByte()
+        if err != nil {
+            return nil, err
+        }
+
+        data = append(data, byt)
+        idx++
+    }
+}
+
+// RecvLine is a Remote method for receiving data until a newline delimiter is detected
+func (r *Remote)RecvLine()([]byte, error) {
+    return r.RecvUntil([]byte("\n"), true)
 }
--- remote_test.go
+++ remote_test.go
@@ -1,21 +1,114 @@
 package sploit;
 
 import(
-	"testing"
+    "net"
+    "time"
+    "testing"
 )
 
 // TestNewRemote tests Remote initialization
 func TestNewRemote(t *testing.T) {
-	r, err := NewRemote("localhost:8000")
-	if err != nil {
-		t.Fatal(err)
-	}
+    // Start a goroutine with the client logic under test
+    go func() {
+        r, err := NewRemote("tcp", "127.0.0.1:8000")
+        if err != nil {
+            t.Fatal(err)
+        }
+        defer r.Close()
 
-	if r.Host != "localhost" {
-		t.Fatal("Host != localhost")
-	}
+        if r.Host != "127.0.0.1" {
+            t.Fatal("Host != localhost")
+        }
 
-	if r.Port != 8000 {
-		t.Fatal("Port != 8000")
-	}
+        if r.Port != 8000 {
+            t.Fatal("Port != 8000")
+        }
+
+        if r.C == nil {
+            t.Fatal("Connection failed")
+        }
+    }()
+
+    // Start a listener server to handle incoming connection
+    l, err := net.Listen("tcp", ":8000")
+    if err != nil {
+        t.Fatal(err)
+    }
+    defer l.Close()
+
+    conn, err := l.Accept()
+    if err != nil {
+        return
+    }
+    defer conn.Close()
+    time.Sleep(1 * time.Second)
+}
+
+// TestRecvLine tests receiving a line of data from a TCP server
+func TestRecvLine(t *testing.T) {
+    go func() {
+        r, err := NewRemote("tcp", "127.0.0.1:8000")
+        if err != nil {
+            t.Fatal(err)
+        }
+        defer r.Close()
+
+        data, err := r.RecvLine()
+        if err != nil {
+            t.Fatal(err)
+        }
+
+        if string(data) != "lolwut" {
+            t.Fatal("Received line != lolwut")
+        }
+    }()
+
+    l, err := net.Listen("tcp", ":8000")
+    if err != nil {
+        t.Fatal(err)
+    }
+    defer l.Close()
+
+    conn, err := l.Accept()
+    if err != nil {
+        return
+    }
+    defer conn.Close()
+    conn.Write([]byte("lolwut\n"))
+    time.Sleep(1)
+}
+
+// TestRecvUntil tests receiving until a specified sequence of bytes
+func TestRecvUntil(t *testing.T) {
+    go func() {
+        r, err := NewRemote("tcp", "127.0.0.1:8000")
+        if err != nil {
+            t.Fatal(err)
+        }
+        defer r.Close()
+
+        data, err := r.RecvUntil([]byte("cmd>"), true)
+        if err != nil {
+            t.Fatal(err)
+        }
+
+        if string(data) != "lolwut\n" {
+            t.Fatal("Received data != lolwut")
+        }
+    }()
+
+    l, err := net.Listen("tcp", ":8000")
+    if err != nil {
+        t.Fatal(err)
+    }
+    defer l.Close()
+
+    conn, err := l.Accept()
+    if err != nil {
+        return
+    }
+    defer conn.Close()
+    conn.Write([]byte("lolwut\n"))
+    conn.Write([]byte("cmd>"))
+    time.Sleep(1)
 }
