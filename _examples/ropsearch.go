4b146854fc63edc3c1a046b08e67478b1417369e
Brandon Miller <brandonkmiller@protonmail.com>
2021-07-17 17:47:31 -0400

    Updated ROP gadget search example
    Now the filepath can be specified to demonstrate both intel and ARM
    gadget searching

--- _examples/ropsearch.go
+++ _examples/ropsearch.go
@@ -1,33 +1,36 @@
-package main;
+package main
 
-import(
-    "github.com/zznop/sploit"
-    "os"
-    "fmt"
+import (
+	"fmt"
+	"github.com/zznop/sploit"
+	"os"
 )
 
-var program = "../test/prog1.x86_64"
-
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Println("./rop_example <regex>")
-        os.Exit(1)
-    }
+	if len(os.Args) < 2 {
+		fmt.Printf("%v <filepath> [regex]\n", os.Args[0])
+		os.Exit(1)
+	}
 
-    elf, _ := sploit.NewELF(program)
-    rop, _ := elf.ROP()
+	regex := ".*"
+	if len(os.Args) >= 3 {
+		regex = os.Args[2]
+	}
 
-    matched, err := rop.InstrSearch(os.Args[1])
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	elf, _ := sploit.NewELF(os.Args[1])
+	rop, _ := elf.ROP()
 
-    matched.Dump()
+	matched, err := rop.InstrSearch(regex)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	matched.Dump()
 }
 
 /*
-$ ./rop_example "pop rbp"
+$ ./ropsearch ../test/prog1.x86_64 "pop rbp"
 0000111f: pop rbp ; ret
 0000111d: add byte ptr [rcx], al ; pop rbp ; ret
 00001118: mov byte ptr [rip + 0x2f11], 1 ; pop rbp ; ret
